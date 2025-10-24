d82231c9cbba29051f58ac351e5037f0e04e5608
Logan Allenstein <ljaicsi25@gmail.com>
2025-10-24 10:45:39 -0500

    Update C# Notes.cs

--- C# Notes.cs
+++ C# Notes.cs
@@ -1,3 +1,5 @@
 // Paste these commands into your VSCode to get the result
-Console.Write("This is an example of a Write command");
 Console.WriteLine("This is an example of a WriteLine command");
+Console.WriteLine("This is to show that this will make a new line");
+Console.Write("This is an example of a Write command");
+Console.Write(": This is to show that this will not make a new line");
